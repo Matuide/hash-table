88e7b8f93d19745595a7ab199b0913663610a8ad
Matthew Coleman <K31073@eltham-college.org.uk>
2020-02-14 12:35:59 +0000

    Add project files.

---
+++ hash table/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace hash_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hash table = new hash();
+            bool continued = true;
+            while (continued == true)
+            {
+            Console.WriteLine("Name:  ");
+            
+            string name;
+            string age;
+            name = Console.ReadLine();
+                Console.WriteLine("age:  ");
+            age = Console.ReadLine();
+            table.AddToTable(name,age);
+                Console.WriteLine("add or print");
+                string answer = Console.ReadLine();
+                if(answer.ToLower() == "add")
+                {
+
+                }
+                else
+                {
+                    continued = false;
+                }
+
+            } 
+            
+
+            table.Print();
+            
+        
+        }
+    }
+}
---
+++ hash table/hash.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hash_table
+{
+    class hash
+    {
+        string[,] tableofHash = new string[10, 2];
+        public void AddToTable(string name,string age)
+        {
+            bool hashed = false;
+            int ASCIIsum = 0, modValue;
+            for(int i = 0; i < name.Length; i++)
+            {
+                ASCIIsum = ASCIIsum + name[i];
+            }
+            modValue = ASCIIsum % 10;
+            while (!hashed)
+            {
+                int count = 0;
+                if (tableofHash[modValue, 0] == null)
+                {
+                    tableofHash[modValue, 0] = name;
+                    tableofHash[modValue, 1] = Convert.ToString(age);
+                    hashed = true;
+                }
+                else
+                {
+                    modValue++;
+                    count++;
+                }
+                if (count == 10)
+                {
+                    hashed = true;
+                    Console.WriteLine("table is full");
+                }
+            }
+        }
+       public void Print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(tableofHash[i, 0] + " : " + tableofHash[i, 1] + " ||| ");
+            }
+        }
+    }
+}
